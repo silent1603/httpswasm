c7d516531e42999dbc28db320ae6e1e9826c78a5
silentnight <trung.daoquoc@gameloft.com>
2025-02-26 19:03:56 +0700

    * add more debug info

--- build_debug_wasm.bat
+++ build_debug_wasm.bat
@@ -14,7 +14,7 @@ echo Template file path: %template_html%
 
 set EMCC_DEBUG=1
 pushd .\bin
-em++ %cpp_files% -fno-rtti -fno-exceptions -s WASM=1 -sMAX_WEBGL_VERSION=2  -s GL_ASSERTIONS   -lGL -s ASSERTIONS=2 -sFILESYSTEM=1 -s SAFE_HEAP=1 -s STACK_OVERFLOW_CHECK=2 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=0 -g3  -gsource-map  -gseparate-dwarf  -gsplit-dwarf --shell-file %template_html% -o index.html
+em++ %cpp_files% -fno-rtti -fno-exceptions -s WASM=1 -sMAX_WEBGL_VERSION=2  -s GL_ASSERTIONS   -lGL -s ASSERTIONS=2 -sFILESYSTEM=1 -s SAFE_HEAP=1 -s STACK_OVERFLOW_CHECK=2 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=0 -g3  -DDEBUG -gsource-map  -gseparate-dwarf  -gsplit-dwarf -fdebug-compilation-dir='..' -o index.html
 popd
 set EMCC_DEBUG=0
 exit /b
