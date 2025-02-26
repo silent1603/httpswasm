843ce7835148d210e5eef52fec14e72d41ca0fef
silentnight <trung.daoquoc@gameloft.com>
2025-02-26 10:28:39 +0700

    * update build script linux

--- build_debug_wasm.sh
+++ build_debug_wasm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cpp_files=$(find ./sources -name "*.cpp" -exec realpath {} \;)
+template_html=$(find ./sources -name "template.html" -exec realpath {} \;)
 pushd ./bin
-em++ $cpp_files -fno-rtti -fno-exceptions -s WASM=1 -sMAX_WEBGL_VERSION=2  -s GL_ASSERTIONS  -lGL -s ASSERTIONS=0 -sFILESYSTEM=1 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=0 -g3 -s USE_FREETYPE=1  -o httpwasm.js
+em++ $cpp_files -fno-rtti -fno-exceptions -s WASM=1 -sMAX_WEBGL_VERSION=2  -s GL_ASSERTIONS  -lGL -s ASSERTIONS=0 -sFILESYSTEM=1 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=0 -g3  --shell-file $template_html -o index.html
 popd
 
-popd
